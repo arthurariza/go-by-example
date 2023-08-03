886d3c3ae46aa12f1de5bbe6918beaa21dc7a34b
arthurariza <tucaariza@gmail.com>
2023-08-03 19:42:44 -0300

    variables

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ func main() {
 	hello_world()
 
 	values()
+
+	variables()
 }
 
 func hello_world() {
@@ -21,4 +23,21 @@ func values()  {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+}
+
+func variables() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
 }
