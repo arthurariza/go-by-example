3ad1ec4f592e2039989970f1000fa9d0772040f0
arthurariza <tucaariza@gmail.com>
2023-08-03 23:33:58 -0300

    if else

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 
 	// constant()
 
-	forr()
+	// forr()
+
+	IfElse()
 }
 
 func hello_world() {
@@ -89,4 +91,27 @@ func forr() {
 		}
 		fmt.Println(n)
 	}
+}
+
+func IfElse() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%2 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+			fmt.Println(num, "has 1 digit")
+	} else {
+			fmt.Println(num, "has multiple digits")
+	}
+ 
+	// No Ternary Operator Go 
+	// 7%2 == 0 ? fmt.Println("7 is even") : fmt.Println("7 is odd")
 }
